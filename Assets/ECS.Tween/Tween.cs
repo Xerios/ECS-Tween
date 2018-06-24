941dcf3facc9ea2387bc3c5814155a9b3e61a237
Sam <sam@megidov.com>
2018-06-24 14:51:51 +0200

    Small refactor

--- Assets/ECS.Tween/Tween.cs
+++ Assets/ECS.Tween/Tween.cs
@@ -39,27 +39,5 @@ namespace ECSTween
 
             return entity;
         }
-
-        /// <summary>
-        /// Tween from one position to another
-        /// </summary>
-        /// <param name="go">GameObject</param>
-        /// <param name="from">From position</param>
-        /// <param name="to">To position</param>
-        /// <param name="time">Interpolation time</param>
-        /// <param name="easing">Easing type</param>
-        public static void Position(GameObject go, Vector3 from, Vector3 to, float time, EasingType easing = EasingType.Linear)
-        {
-            var entityManager = World.Active.GetExistingManager<EntityManager>();
-
-            var entity = BaseTween(go, time, easing);
-            entityManager.AddComponentData(entity, new Position(from));
-            entityManager.AddComponentData(entity, new CopyTransformToGameObject());
-            entityManager.AddComponentData(entity, new TweenPosition()
-            {
-                From = from,
-                To = to
-            });
-        }
     }
 }
